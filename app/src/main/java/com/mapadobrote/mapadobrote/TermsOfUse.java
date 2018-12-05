c1596e93a57aefb8d57d21edb3d6fee2f246c0af
Vukasin Simic <vukasin.simic@gmail.com>
2018-12-05 12:59:53 +0100

    TermsOfUse - layout created

---
+++ app/src/main/java/com/mapadobrote/mapadobrote/TermsOfUse.java
@@ -0,0 +1,14 @@
+package com.mapadobrote.mapadobrote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TermsOfUse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_use);
+        
+    }
+}
