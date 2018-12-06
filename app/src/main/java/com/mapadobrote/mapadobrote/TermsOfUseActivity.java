ed9febb373b4e10e974aa3cdfc43b032c44e9183
Vukasin Simic <vukasin.simic@gmail.com>
2018-12-06 12:08:36 +0100

    TermsOfUse - layout created and logic

---
+++ app/src/main/java/com/mapadobrote/mapadobrote/TermsOfUseActivity.java
@@ -0,0 +1,32 @@
+package com.mapadobrote.mapadobrote;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class TermsOfUseActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_use);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        SharedPreferences sharedPreferences = getSharedPreferences("agree", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("agree", true);
+        editor.commit();
+        if (editor.commit() == true) {
+            Intent intent = new Intent(this, ChooseCategoryActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
