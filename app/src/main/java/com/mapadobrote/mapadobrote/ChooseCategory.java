e2ef093e333dd65a01d6d02f16e0f84f791dd7f6
Vukasin Simic <vukasin.simic@gmail.com>
2019-01-08 09:21:22 +0100

    Location and Google Map created

---
+++ app/src/main/java/com/mapadobrote/mapadobrote/ChooseCategory.java
@@ -0,0 +1,13 @@
+package com.mapadobrote.mapadobrote;
+
+public class ChooseCategory {
+    public final String category;
+    public final String imageUrl;
+    public final int id;
+
+    ChooseCategory(String category, String imageUrl, int id) {
+        this.category = category;
+        this.imageUrl = imageUrl;
+        this.id = id;
+    }
+}
